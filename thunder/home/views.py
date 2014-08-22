8e98c3c0e222efbf45e78f3e695e5fd3f8a9041d
neo <heziqiang@qq.com>
2014-08-22 15:44:20 +0800

    json

--- thunder/home/views.py
+++ thunder/home/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
-# from django.template import loader,Context
+from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
-import json
+from django.utils import simplejson
+#import json
 from models import Account
+from django.forms.models import model_to_dict
 
 def  index(req):
 	return render_to_response('index.html')
@@ -11,5 +11,6 @@ def  index(req):
 def acc(req,page):
 	acc_list = Account.objects.all()
 	test_acc_list = acc_list[0:5]
-	response_data = {'accounts': test_acc_list}
-	return HttpResponse(json.dumps(response_data), content_type="application/json")
+	results = [ob.as_json() for ob in test_acc_list]
+	response_data = {'accounts': results}
+	return HttpResponse(simplejson.dumps(response_data), content_type="application/json")
